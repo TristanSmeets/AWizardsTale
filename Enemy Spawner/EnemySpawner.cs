f34b9e41962ec0c7bc86314eb428817535c3ea09
Tristan Smeets <tristansmeets89@hotmail.com>
2019-04-18 14:52:04 +0200

    Added the enemy spawner i made.

---
+++ Enemy Spawner/EnemySpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Spawner))]
+public class EnemySpawner : MonoBehaviour {
+
+	public GameObject Enemy;
+
+	[SerializeField]
+	float cooldownTime = 1.5f, timeOffset = 0.5f;
+	Spawner spawner;
+
+	void Start () {
+		spawner = gameObject.GetComponent<Spawner>();
+		WaveStartEvent.Register(onWaveStart);
+	}
+
+	void onWaveStart(WaveStartEvent waveStartEvent)
+	{
+		GameObject CrystalFragment = waveStartEvent.CrystalFragment;
+		int AmountOfEnemies = waveStartEvent.AmountOfEnemies;
+		spawner.SpawnMultipleObjects(Enemy,
+			CrystalFragment.transform.position, 
+			CrystalFragment.transform.rotation,
+			AmountOfEnemies, 
+			cooldownTime, 
+			timeOffset);
+	}
+}
---
+++ Enemy Spawner/Spawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+	IEnumerator coroutine;
+
+	/// <summary>
+	/// Spawn function that takes 4 parameters:
+	/// - GameObject it needs to spawn.
+	/// - Vector3 for the spawn position.
+	/// - Quaternion for the rotation.
+	/// - int for the amount of objects it needs to spawn.
+	/// - float for the time between objects. 
+	/// - float for the offset for the randomisation.
+	/// </summary>
+	/// <param name="objectToSpawn"></param>
+	/// <param name="position"></param>
+	/// <param name="rotation"></param>
+	/// <param name="amountToSpawn"></param>
+	/// <param name="cooldownTime"></param>
+	public void SpawnMultipleObjects(GameObject objectToSpawn, Vector3 position, Quaternion rotation, int amountToSpawn, float cooldownTime, float offset)
+	{
+		coroutine = SpawnObjects(objectToSpawn, position, rotation, amountToSpawn, cooldownTime, offset);
+		StartCoroutine(coroutine);
+	}
+
+	IEnumerator SpawnObjects(GameObject objectToSpawn, Vector3 position, Quaternion rotation, int amountOfEnemies, float cooldownTime, float offset)
+	{
+		for (int iteration = 0; iteration < amountOfEnemies; iteration++)
+		{
+			Instantiate(objectToSpawn, position, rotation);
+			yield return new WaitForSeconds(Random.Range(cooldownTime - offset, cooldownTime + offset));
+		}
+	}
+}
